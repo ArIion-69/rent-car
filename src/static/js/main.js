fa3c42f569e32af520cf0e16fe772001325ef3be
Danil <Lachkow087@gmai.com>
2024-07-17 15:22:53 +0500

    доделаны блоки

---
+++ src/static/js/main.js
@@ -0,0 +1,12 @@
+const swiperAuthor = new Swiper('.reviews-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
